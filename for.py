eadc6a5fba34365c45c6ddab2abe28e03db45a99
eli18 <el.pla@duocuc.cl>
2026-04-16 13:15:05 -0400

    mas ejercicios

---
+++ for.py
@@ -0,0 +1,14 @@
+# expliacion de for
+
+#for i in range(3):
+ #   print("hola")
+
+notas=int(input/"ingrese la cantidad de notas:")
+suma=0
+for i in range (notas): 
+ n=float (f"input("ingrese la nota{i+1}")
+ suma=suma+n 
+ prom=suma/notas 
+ print("el promedio es", round (prom, 1))
+  if prom>=4:
+print("alumno aprobado")
---
+++ inicio.py
@@ -0,0 +1,35 @@
+# num=int(input("ingrese un numero:"))
+# for i in range (num):
+
+#     print ("hola camello")
+#     print(f"{i+1} hola camello")
+    
+    
+#     for i in "diego": 
+#      print(i)
+
+nombre=input("igrese nombre")
+vocales=0 
+cons=0 
+
+for i in nombre: 
+ print(i)
+ vocales=vocales+1 
+if i in "aeiou":
+ vocales+=1 
+ cons+=1 
+ print(f" cant de consonantes {cons}") 
+ 
+       
+
+
+
+    
+
+       
+       
+          
+
+
+
+
