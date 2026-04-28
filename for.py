5bc812ea7b3d2a17a620b3e6ee8ccf7d2276a7ff
eli18 <el.pla@duoc>
2026-04-28 13:33:18 -0400

    cambios

--- for.py
+++ for.py
@@ -3,12 +3,12 @@
 #for i in range(3):
  #   print("hola")
 
-notas=int(input/"ingrese la cantidad de notas:")
-suma=0
-for i in range (notas): 
- n=float (f"input("ingrese la nota{i+1}")
- suma=suma+n 
- prom=suma/notas 
- print("el promedio es", round (prom, 1))
-  if prom>=4:
-print("alumno aprobado")
+#notas=int(input/"ingrese la cantidad de notas:")
+#suma=0
+#for i in range (notas): 
+ #n=float (f"input("ingrese la nota{i+1}")
+ #suma=suma+n 
+ #prom=suma/notas 
+ #print("el promedio es", round (prom, 1))
+  #if prom>=4:
+#print("alumno aprobado")
---
+++ randomly.py
@@ -0,0 +1,84 @@
+#uso y ejemplos de random 
+
+import random 
+import time 
+
+#print(random.randint(1,10))
+
+#for i in range(num):
+ #   print("hola deny")  
+#dado=random.randint(1,6) 
+#dado=random.randint(1,6)
+
+#print(f"el dado 1 dio {dado1}")
+#print(f"el dado 2 dio {dado2}")
+
+#if dado1==dado2:
+
+
+#adivina el numero  
+# for i in range(num): 
+# numrandom=randint(1,100): 
+# if adivinar>numrandom 
+
+# print(f"el num dio {num}")
+# for i in range (5): 
+#     print ("el numero es menor")
+# else
+#     print ("el numero es mayor ")
+
+#ludo 
+# posicion=0
+# meta=30 
+# turnos=1 
+# while posicion<meta:
+
+#     dado1= random.randint(1,6)
+#     dado2= random.randint(1,6)
+#     # print(f"el dado 1 dio {dado1}") 
+#     # print(f"el dado 2 dio {dado2}")
+    
+
+
+#     time.sleep(1)
+#     posicion+=dado1+dado2 
+#     print (f"sacaste {dado1} y {dado2}") 
+#     print(f"el usuario esta en la posicion {posicion} ")
+
+# 10 -40
+
+# j1=random.randint(10-40)
+# j2=random.randint(10-40)
+# j3=random.randint (10-40)
+# print(f"el jugador 1 lanzo la pelota {j1} metros")
+# print(f"el jugador 2 lanzo la pelota {j2} metros")
+# print (f"el jugador 3 lanzo la pelota {j3} metros")
+
+# if j1>j2 and j1>j3 
+# print ("el jugador uno, lanzo la pelota mas lejos")
+# elif j2>j3: 
+# print ("el jugador dos, lanzo la pelota mas lejos")
+# elif 
+# print("el jugador tres, lanzo la pelota mas lejos")
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+   
+      
+
+    
